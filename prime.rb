e3f8be418abe29f9754e842b6f4368e14dd26336
Wendi Chen <epanistamai.proles@gmail.com>
2018-11-22 21:32:39 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,9 +2,13 @@
 def prime?(num)
   fin_num = num - 1
   arr=*(2..fin_num)
-  if num < 2
-    return "false"
-  else
-    return "true"
+    if num < 2
+      return "false"
+    elsif num == 2
+      return "true"
+    arr.each do |i| 
+      return false if number % x == 0
+    end
+   returns true
   end
 end
