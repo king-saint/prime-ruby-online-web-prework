ac3667c7a72e5b33adad6136855c89bdaa01323a
Wendi Chen <epanistamai.proles@gmail.com>
2018-11-22 21:22:11 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
 # Add  code here!
+def prime?(num)
+  prime = [2, 3, 5, 7, 11]
+    prime.each for |i| do
+       num % i == 0 ? return "false" : return true
+       
+       end 
+      
+    end 
+  end
