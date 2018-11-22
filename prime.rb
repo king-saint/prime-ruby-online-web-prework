bcaa197875ec0b8e305fed3ad6889f0846b28244
Wendi Chen <epanistamai.proles@gmail.com>
2018-11-22 21:35:47 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,9 @@
 # Add  code here!
-def prime?(num)
+def prime?(int)
 
-    return false if num < 2
-    return true if num == 2
-    (2..int - 1).each do |i| 
+    return false if int < 2
+    return true if int == 2
+    (2..number - 1).each do |i| 
       return false if int % i == 0
     end
    returns true
