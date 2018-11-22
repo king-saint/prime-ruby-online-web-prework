018307e206765008f65f24fb332653ef76420aa7
Wendi Chen <epanistamai.proles@gmail.com>
2018-11-22 21:23:38 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@
 def prime?(num)
   prime = [2, 3, 5, 7, 11]
     prime.each for |i| do
-       num % i == 0 ? return "false" : return true
+      num >= 2 && num % i == 0 ? return "false" : return true
        
        end 
       
