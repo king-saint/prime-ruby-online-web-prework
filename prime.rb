631046cee967c5c26f5dcf6eb8fd5946cd3c0f78
Wendi Chen <epanistamai.proles@gmail.com>
2018-11-22 21:25:56 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,7 @@
 # Add  code here!
 def prime?(num)
-  prime = [2, 3, 5, 7, 11]
-    prime.each for |i| do
-      if num >= 2 && num % i == 0 
-        return "false" 
-      else
-        return "true"
-       
-       end 
-      
-    end 
-  end
+  if num < 2
+    return "false"
+  elsif 2..num-1
+  
+end
