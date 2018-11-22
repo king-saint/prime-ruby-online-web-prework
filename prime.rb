038746edc833a7be8d3e9888ba957cf88722ff67
Wendi Chen <epanistamai.proles@gmail.com>
2018-11-22 21:34:50 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,10 @@
 # Add  code here!
 def prime?(num)
 
-    return false if int < 2
-    return true if int == 2
-    (2..num - 1).each do |i| 
-      return false if number % x == 0
+    return false if num < 2
+    return true if num == 2
+    (2..int - 1).each do |i| 
+      return false if int % i == 0
     end
    returns true
 end
